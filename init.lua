047536d9d18d27d3025c2b83a52b79e02f7a7891
Alberto-Izquierdo <alberto.izquierdo.ramirez@gmail.com>
2024-04-29 20:19:59 +0200

    Added go and rust lsps

--- init.lua
+++ init.lua
@@ -557,11 +557,11 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
         -- pyright = {},
         clangd = {},
         pylsp = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
